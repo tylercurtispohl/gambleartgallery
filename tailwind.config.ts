e883a93d31257feed33b93687823c18b45a5be23
Tyler Pohl <tylercurtispohl@gmail.com>
2024-01-24 13:12:26 -0800

    add more tailwind responsive breakpoints and heights

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,50 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: (theme) => ({
+        "1/16": "calc(100vh / 16)",
+        "2/16": "calc((100vh / 16) * 2)",
+        "3/16": "calc((100vh / 16) * 3)",
+        "10/16": "calc((100vh / 16) * 10)",
+        "12/16": "calc((100vh / 16) * 12)",
+        "13/16": "calc((100vh / 16) * 13)",
+        "15/16": "calc((100vh / 16) * 15)",
+      }),
+      screens: {
+        "hover-hover": { raw: "(hover: hover)" },
+      },
+    },
+    screens: {
+      "xs-max": { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+
+      "3xl": "1792px",
+      // => @media (min-width: 1536px) { ... }
+
+      "4xl": "2048px",
+      // => @media (min-width: 1536px) { ... }
+
+      "5xl": "2304px",
+      // => @media (min-width: 1536px) { ... }
+
+      "6xl": "2560px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
