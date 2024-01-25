635243f6ec45c5db3ac5122dfc2e3d322984ec2b
Tyler Pohl <tylercurtispohl@gmail.com>
2024-01-24 16:18:04 -0800

    Add contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <main className="">contact</main>;
+export default function Page() {
+  return (
+    <div className="flex justify-around text-blue-900">
+      <div className="flex flex-col justify-start text-center text-lg tracking-wide w-full">
+        <p>Kirsten S Gamble</p>
+        <p>145 Town Center #683</p>
+        <p>Corte Madera, CA 94925</p>
+        &nbsp;
+        <a href="mailto:kirstengamble145@gmail.com" className="underline">
+          kirstengamble145@gmail.com
+        </a>
+      </div>
+    </div>
+  );
 }
