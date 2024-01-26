fab759f13bb5ef09fb91d4203b233bdc13bbecc7
Tyler Pohl <tylercurtispohl@gmail.com>
2024-01-25 16:29:20 -0800

    add phone number back

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,6 +6,7 @@ export default function Page() {
         <p>145 Town Center #683</p>
         <p>Corte Madera, CA 94925</p>
         &nbsp;
+        <p>(415) 816-2155</p>
         <a href="mailto:kirstengamble145@gmail.com" className="underline">
           kirstengamble145@gmail.com
         </a>
