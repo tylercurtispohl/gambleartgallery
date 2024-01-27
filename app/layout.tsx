42f93442cec18e2fa3b4111f31bddf523eb6edd8
Tyler Pohl <tylercurtispohl@gmail.com>
2024-01-27 12:05:18 -0800

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Nav } from "./ui/nav";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
             </div>
           </main>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
